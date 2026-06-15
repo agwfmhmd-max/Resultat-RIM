2a46d659ce58646bb646eb64ede200449da879ac
Mohamed Dah Agove <agwfmhmd@gmail.com>
2026-06-15 16:11:51 +0000

    Change CACHE_NAME from v19 to v20
    Updated CACHE_NAME to force browser refresh.

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // تغيير الرقم هنا هو ما يجبر المتصفح على التحديث
-// غيرناه من v18 إلى v18
-const CACHE_NAME = 'resultat-rim-v19';
+// غيرناه من v19 إلى v20
+const CACHE_NAME = 'resultat-rim-v20';
 
 // قائمة الملفات التي سيتم حفظها للعمل بدون إنترنت
 const ASSETS_TO_CACHE = [
