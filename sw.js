a6d3370fad7e91ff07e662f0f7815f583a7ede74
Mohamed Dah Agove <agwfmhmd@gmail.com>
2026-06-15 15:33:16 +0000

    Update CACHE_NAME from v18 to v19

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // تغيير الرقم هنا هو ما يجبر المتصفح على التحديث
-// غيرناه من v17 إلى v18
-const CACHE_NAME = 'resultat-rim-v18';
+// غيرناه من v18 إلى v18
+const CACHE_NAME = 'resultat-rim-v19';
 
 // قائمة الملفات التي سيتم حفظها للعمل بدون إنترنت
 const ASSETS_TO_CACHE = [
