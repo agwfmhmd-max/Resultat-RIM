ac043cd3b1525a893e219452aa877142a6ea8f6c
Mohamed Dah Agove <agwfmhmd@gmail.com>
2026-07-08 18:56:28 +0000

    Update CACHE_NAME for service worker
    Updated CACHE_NAME from 'resultat-rim-v25' to 'resultat-rim-v27'.

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // تغيير الرقم هنا هو ما يجبر المتصفح على التحديث
-// غيرناه من v24 إلى v25
-const CACHE_NAME = 'resultat-rim-v25';
+// غيرناه من v26 إلى v27
+const CACHE_NAME = 'resultat-rim-v27';
 
 // قائمة الملفات التي سيتم حفظها للعمل بدون إنترنت
 const ASSETS_TO_CACHE = [
